cf8af6ec820536a9aa8da0b52aeba71a33e94cda
Jeff Holman <jeff.holman@yahoo.com>
2017-04-17 15:12:45 -0400

    Daily

--- hlcWeb/ViewModels/DoctorViewModel.cs
+++ hlcWeb/ViewModels/DoctorViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace hlcWeb.ViewModels
@@ -12,25 +9,41 @@ namespace hlcWeb.ViewModels
 
         [Required]
         [StringLength(50)]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [StringLength(50)]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         public int PracticeId { get; set; }
 
         [StringLength(12)]
+        [DisplayName("Mobile Phone")]
         public string MobilePhone { get; set; }
 
         [StringLength(12)]
+        [DisplayName("Home Phone")]
         public string HomePhone { get; set; }
 
         [StringLength(12)]
+        [DisplayName("Pager")]
         public string Pager { get; set; }
 
         [StringLength(80)]
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
 
+        [DisplayName("Member of BSMP")]
+        // ReSharper disable once InconsistentNaming
+        public bool IsBSMP { get; set; }
+
+        [DisplayName("High Risk Pregnancy doctor")]
+        // ReSharper disable once InconsistentNaming
+        public bool IsHRP { get; set; }
+
+        [DisplayName("Peer review")]
+        public string PeerReview { get; set; }
     }
 }
