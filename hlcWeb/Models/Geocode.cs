0ef8dfe6e06563fbfd2914f802d4745a1273c47d
Jeff Holman <jeffrey.holman@duke-energy.com>
2019-03-27 20:03:53 -0400

    Added Geocode.cs

---
+++ hlcWeb/Models/Geocode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hlcWeb.Models
+{
+    public class Geocode
+    {
+        public string Status { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+
+    public class GeocodeRequest
+    {
+        public string location { get; set; }
+        public GeocodeRequestOptions options;
+    }
+
+    public class GeocodeRequestOptions
+    {
+        public string thumbMaps { get; set; }
+    }
+}
